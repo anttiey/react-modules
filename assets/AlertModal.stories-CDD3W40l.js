36ec5c712a4e9984c63e029724a4d55174878249
anttiey <anttiey@users.noreply.github.com>
2024-05-15 00:29:48 +0000

    deploy: 5304aa3cea7334e3fab1bd28e041fe1664004cdc

---
+++ assets/AlertModal.stories-CDD3W40l.js
@@ -0,0 +1,7 @@
+import{A as a}from"./AlertModal-B4imaCWV.js";import"./jsx-runtime-QvZ8i92b.js";import"./index-uubelm5h.js";import"./ModalLayout-DjCzfzRt.js";import"./index-Dei0BBcc.js";import"./Title-CH_s7XQj.js";import"./Button-ClUyEfuz.js";const l={title:"Modal/AlertModal",component:a,parameters:{controls:{exclude:["close"]}}},e={args:{headerText:"알림 모달 제목",bodyText:"알림 모달 내용",isOpen:!0}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    headerText: '알림 모달 제목',
+    bodyText: '알림 모달 내용',
+    isOpen: true
+  }
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const i=["Default"];export{e as Default,i as __namedExportsOrder,l as default};
---
+++ assets/PromptModal.stories-Cq0mE9My.js
@@ -0,0 +1,6 @@
+import{P as a}from"./PromptModal-CGOn6C3Z.js";import"./jsx-runtime-QvZ8i92b.js";import"./index-uubelm5h.js";import"./ModalLayout-DjCzfzRt.js";import"./index-Dei0BBcc.js";import"./Title-CH_s7XQj.js";import"./Input-E5SnQE0W.js";import"./Button-ClUyEfuz.js";const l={title:"Modal/PromptModal",component:a,parameters:{controls:{exclude:["inputValue","onInputChange","onConfirm","close"]}}},e={args:{headerText:"프롬프트 모달 제목",isOpen:!0}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    headerText: '프롬프트 모달 제목',
+    isOpen: true
+  }
+}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const C=["Default"];export{e as Default,C as __namedExportsOrder,l as default};
